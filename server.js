6868336d1d2d2b515ce7c04108aedfa50801c924
papamw <wadepapa@gmail.com>
2025-02-08 23:01:46 +0000

    "Mise à jour package.json"

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("www"));
+
+var server = app.listen(8000, function () {
+  var host = "localhost";
+  var port = 8000;
+  console.log("Express app listening at http://%s:%s", host, port);
+});
